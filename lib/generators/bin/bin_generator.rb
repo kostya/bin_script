c47c7528f7529f1958fdc900d0ebd2c2a59a5841
Konstantin Makarchev <kostya27@gmail.com>
2012-08-22 03:06:48 +0400

    # generate short bin (without rb)

--- lib/generators/bin/bin_generator.rb
+++ lib/generators/bin/bin_generator.rb
@@ -6,10 +6,10 @@ if Rails::VERSION::MAJOR >= 3
       source_root File.expand_path("../templates", __FILE__)
 
       def add_files
-        template "script.rb", "bin/#{file_path}.rb"
+        template "script.rb", "bin/#{file_path}"
         template "script_class.rb", "app/models/bin/#{file_path}_script.rb"
         template "spec.rb", "spec/models/bin/#{file_path}_script_spec.rb"
-        chmod "bin/#{file_path}.rb", 0755
+        chmod "bin/#{file_path}", 0755
       end
     end
   end
@@ -22,7 +22,7 @@ if Rails::VERSION::MAJOR == 2
   class BinGenerator < Rails::Generator::NamedBase
     def manifest
       record do |m|
-        m.template "script.rb", "bin/#{file_path}.rb", :chmod => 0755
+        m.template "script.rb", "bin/#{file_path}", :chmod => 0755
         m.template "script_class.rb", "app/models/bin/#{file_path}_script.rb"
         m.directory "spec/models/bin"
         m.template "spec.rb", "spec/models/bin/#{file_path}_script_spec.rb"
